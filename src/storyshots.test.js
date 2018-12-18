5aadb5218e2a4d6687c45f26a77a564f68211f5f
ahlumin <magus52941@gmail.com>
2018-12-18 18:26:41 +0800

    add snapshot with storybook via jest

---
+++ src/storyshots.test.js
@@ -0,0 +1,8 @@
+
+/**
+ * Take reference about the configuration API, see below:
+ * https://github.com/infinitered/addon-storyshots
+ */
+
+import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
+initStoryshots();
