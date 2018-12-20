bed3433be0bc64c545feb75a1293af01d6fec46d
ahlumin <magus52941@gmail.com>
2018-12-20 14:59:41 +0800

    refactor SlideBar and add unit test

--- src/components/SlideBar/index.js
+++ src/components/SlideBar/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import style from './style.scss';
 const cx = classnames.bind(style);
 
 export default class SlideBar extends React.Component {
-  state = {
-    offsetLeft: 0,
-    offsetWidth: 0,
-    maxPositionX: 0,
-    fillPercent: 0,
+  constructor(props) {
+    super(props);
+    const { value, max, min,} = props;
+    const fillPercent = value >= max ? 100 : value <= min ? 0 : Math.floor((value / max) * 100);
+
+    this.state = {
+      offsetLeft: 0,
+      offsetWidth: 0,
+      maxPositionX: 0,
+      fillPercent,
+    }
   }
+
+  static propTypes = {
+    max: PropTypes.number,
+    min: PropTypes.number,
+    value: PropTypes.number,
+    onDrag: PropTypes.func,
+  }
+
+  static defaultProps = {
+    max: 100,
+    min: 0,
+    value: 0,
+  }
+
+  slider = React.createRef();
 
   componentDidMount() {
     const {
       offsetLeft,
       offsetWidth,
-    } = this.slider;
+    } = this.slider.current;
 
     this.setState({
       offsetLeft,
@@ -31,14 +53,17 @@ export default class SlideBar extends React.Component {
 
   handleDrag = e => {
     e.stopPropagation();
+    const { max, onDrag, } = this.props;
     const pageX = e.pageX || e.changedTouches[0].pageX;
     const { offsetLeft, offsetWidth, maxPositionX, } = this.state;
-    const moveDistance = pageX <= offsetLeft ? 0 :
-      pageX >= maxPositionX ? offsetWidth : pageX - offsetLeft;
-    const value = Math.floor((moveDistance / offsetWidth) * 100);
+    const moveDistance = pageX <= offsetLeft ? 0 :pageX >= maxPositionX ? offsetWidth : pageX - offsetLeft;
+    const fillPercent = Math.floor((moveDistance / offsetWidth) * 100);
 
+    const newValue = Math.floor((fillPercent / 100) * max);
     this.setState({
-      fillPercent: value,
+      fillPercent,
+    }, () => {
+      onDrag && onDrag(newValue, e);
     });
   }
 
@@ -52,10 +77,7 @@ export default class SlideBar extends React.Component {
     const { fillPercent, } = this.state;
     return (
       <div
-        ref={el => {
-          this.slider = el;
-        }
-        }
+        ref={this.slider}
         className={cx('slider')}
       >
         <span
--- src/components/SlideBar/story.js
+++ src/components/SlideBar/story.js
@@ -7,3 +7,7 @@ const stories = storiesOf('SlideBar', module);
 stories.add('a normal component', () => (
   <SlideBar />
 ));
+
+stories.add("with parent's state", () => (
+  <SlideBar value={5} max={1000} onDrag={(newValue) => { console.log(newValue) }} />
+));
