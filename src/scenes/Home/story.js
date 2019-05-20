459a55a90f761081d7005693033ccbc4e4ca1ba0
Ahlumin <magus52941@gmail.com>
2019-05-20 10:34:22 +0800

    Update snapshot

--- src/scenes/Home/story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Home from './index.js';
-
-storiesOf('Home', module)
-  .add('a normal component', () => (
-    <Home />
-  ));
